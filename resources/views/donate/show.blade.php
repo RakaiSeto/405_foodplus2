e3a7cd0c3b3551c0b8c1ed2e5652ff13c5dbd517
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2025-05-24 16:22:03 +0700

    feat(ShowDonation): add ui image on show donation

--- resources/views/donate/show.blade.php
+++ resources/views/donate/show.blade.php
@@ -92,6 +92,11 @@
             <div class="content">
                 <div class="detail-container">
                     <div class="detail-item">
+                        <div class="detail-label">Gambar Makanan</div>
+                        <div class="detail-value">
+                            <img src="{{ asset("storage/" . $donation->image_url)  }}" /></div>
+                    </div>
+                    <div class="detail-item">
                         <div class="detail-label">Nama Makanan</div>
                         <div class="detail-value">{{ $donation->food_name }}</div>
                     </div>
