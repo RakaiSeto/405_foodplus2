6d994dac15c339150c1d9a78673bc7f56c8c6d0c
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2025-05-29 07:30:43 +0700

    ref(seed): Make sure admin is created first

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+         User::factory()->create([
+            "name" => "admin",
+            "email" => "admin@gmail.com",
+            "role" => "admin",
+            "password" => Hash::make("admin")
+        ]);
         User::factory()->create([
             "name" => "test resto",
             "email" => "testresto@gmail.com",
@@ -26,11 +32,6 @@ class DatabaseSeeder extends Seeder
             "role" => "penerima",
             "password" => Hash::make("password")
         ]);
-        User::factory()->create([
-            "name" => "admin",
-            "email" => "admin@gmail.com",
-            "role" => "admin",
-            "password" => Hash::make("admin")
-        ]);
+
     }
 }
